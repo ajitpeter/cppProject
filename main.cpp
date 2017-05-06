474ed7fe6f287c1762c91a5618230f894795b058
Ajit <ajitpeter86@gmail.com>
2017-05-06 21:59:28 +0100

    Add vector to existing code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#incldue <vector>
 
 int main()
 {
 	std::cout<<"Travis CI\n";
+	std::vector<int> v = { 1, 2, 3, 4, 5, 6 };
+	
+	for(auto eachVal : v)
+		std::cout << eachVal;
+		
 	return 0;
 }
