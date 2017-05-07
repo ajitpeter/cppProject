713c37af01e2f9809079507da986bbd9b1b832d0
Ajit <ajitpeter86@gmail.com>
2017-05-07 15:35:18 +0100

    Logical errors fixed

--- main.cpp
+++ main.cpp
@@ -2,11 +2,9 @@
 
 int main()
 {
-	int a = 10; 
-	int b = 20;
-	int c = a + b;
-	
-	printf("The sum is : {0}"+c);
+	int a = 10, b = 20, c;
+	c = a + b;
+	printf("The sum is : %d", c);
 		
 	return 0;
 }
