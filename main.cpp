380a687378105469bc1e6ebda6dbd7ebe603bd06
Ajit <ajitpeter86@gmail.com>
2017-05-07 14:53:48 +0100

    Basic program with single output

--- main.cpp
+++ main.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
-#incldue <vector>
-
-template <typename T>
-void sum(T a, T b)
-{
-	return a + b;
-}
 
 int main()
 {
 	std::cout<<"Travis CI\n";
-	std::vector<int> v = { 1, 2, 3, 4, 5, 6 };
 	
-	for(auto eachVal : v)
-		std::cout << eachVal;
-		
-	std::cout << "\n" << sum(5, 10);
-		
 	return 0;
 }
