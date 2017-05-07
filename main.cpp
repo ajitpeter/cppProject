71fcb7f4dada60b347723870cf2a83496e9cf74b
Ajit <ajitpeter86@gmail.com>
2017-05-07 15:06:15 +0100

    Add a sample vector

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
 	std::cout<<"Travis CI\n";	
+	std::vector<int> v = { 1, 2, 3, 4, 5, 6 };
+	
+	for(auto val : v)
+		std::cout << val << "\n";
+		
 	return 0;
 }
