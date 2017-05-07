81dde59f22e1f1a3377c77e24e4d8eca03ac8dae
Ajit <ajitpeter86@gmail.com>
2017-05-07 15:43:31 +0100

    Revert to initial source with template and vector

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-#include <stdio.h>
+#include <iostream>
+#incldue <vector>
+
+template <typename T>
+void sum(T a, T b)
+{
+	return a + b;
+}
 
 int main()
 {
-	int a = 10, b = 20, c;
-	c = a + b;
-	printf("The sum is : %d", c);
+	std::cout<<"Travis CI\n";
+	std::vector<int> v = { 1, 2, 3, 4, 5, 6 };
+	
+	for(auto eachVal : v)
+		std::cout << eachVal;
+		
+	std::cout << "\n" << sum(5, 10);
 		
 	return 0;
 }
